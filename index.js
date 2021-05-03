74823cf581d6f7046644ad606af8e0e5c7353010
Nour El Houda Sarmout <nourelhoudasarmout@gmail.com>
2021-05-03 09:01:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,3 +16,25 @@ function wrapAdjective(s="*") {
 }
 
 const encouragingPromptFunction = wrapAdjective("!!!");
+
+const Calculator = {
+  add: function(a, b) {
+    return a+b;
+  },
+  subtract: function(a, b) {
+    return a-b;
+  },
+  multiply: function(a, b) {
+    return a*b;
+  },
+  divide: function(a, b) {
+    return a/b;
+  }
+}
+
+function actionApplyer(start, fns) {
+  for (let i = 0; i < fns.length; i++) {
+    start = fns[i](start);
+  }
+  return start;
+}
