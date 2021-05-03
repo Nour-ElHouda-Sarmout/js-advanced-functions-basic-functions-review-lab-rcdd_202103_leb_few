5ba421bbc92a720128e580e6c58e774cb4d300f6
Nour El Houda Sarmout <nourelhoudasarmout@gmail.com>
2021-05-03 08:52:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+function saturdayFun(activity="roller-skate"){
+  return `This Saturday, I want to ${activity} !`;
+}
+saturdayFun();
+
+const mondayWork = function(activity="go to the office"){
+  return `This Monday, I will ${activity} .`;
+};
+mondayWork();
+
+function wrapAdjective(s="*") {
+  return function(adjective="special") {
+    return `You are ${adjective}`
+  }
+}
+
+const encouragingPromptFunction = wrapAdjective("!!!");
