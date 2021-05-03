6b2eb473c6186cb03ce6a6db271151d934c3738e
Nour El Houda Sarmout <nourelhoudasarmout@gmail.com>
2021-05-03 08:54:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Your code here
 function saturdayFun(activity="roller-skate"){
-  return `This Saturday, I want to ${activity} !`;
+  return `This Saturday, I want to ${activity}!`;
 }
 saturdayFun();
 
 const mondayWork = function(activity="go to the office"){
-  return `This Monday, I will ${activity} .`;
+  return `This Monday, I will ${activity}.`;
 };
 mondayWork();
 
